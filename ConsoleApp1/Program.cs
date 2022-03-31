92f236bd8c9cf9ba3589f2c162c839d895b222ff
acomais <acomais@ACO-DEVJR01>
2022-03-31 09:51:07 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1 
+{
+    class Program 
+    {
+        public static void Main(string[] args) 
+        {
+            int numero;
+
+            Console.WriteLine("Digite um numero de 1 a 7 para informar o dia da semana");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero >= 2 && numero <= 6)
+            {Console.WriteLine("É dia útil");}
+
+            else if (numero == 1 || numero == 7)
+            {Console.WriteLine("Não é dia útil");}
+
+        } 
+    }
+}
